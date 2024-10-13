405b0faa80da86f1a6f1034ba2f453a4cf4c930e
igrisdev <johanalvarezz237@gmail.com>
2024-10-13 00:27:43 -0500

    quitando vercel para SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,8 @@ import tailwind from '@astrojs/tailwind'
 
 import react from '@astrojs/react'
 
-import vercel from '@astrojs/vercel/serverless'
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   // output: 'server',
-  // adapter: vercel(),
 })
