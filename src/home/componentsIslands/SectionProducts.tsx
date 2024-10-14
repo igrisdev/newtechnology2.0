1a6bc41ba6ffc931c8f3c28609c6f4bba6343add
igrisdev <johanalvarezz237@gmail.com>
2024-10-13 21:38:26 -0500

    Todos los productos

--- src/home/componentsIslands/SectionProducts.tsx
+++ src/home/componentsIslands/SectionProducts.tsx
@@ -66,6 +66,29 @@ export const SectionProducts = ({
             {name}
           </button>
         ))}
+        <a
+          href="/products"
+          className="px-7 py-3 uppercase border-local_button border-[1px] font-bold text-sm text-local_text hover:bg-local_accent rounded-md hover:text-white flex items-center gap-2"
+        >
+          Ver Todos
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="w-5 h-5"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M5 12l14 0" />
+            <path d="M13 18l6 -6" />
+            <path d="M13 6l6 6" />
+          </svg>
+        </a>
       </div>
 
       <div id="products">
