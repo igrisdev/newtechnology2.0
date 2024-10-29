0736f4f0940b8dbb05a89f8000f69c22a1f35991
igrisdev <johanalvarezz237@gmail.com>
2024-10-29 13:51:24 -0500

    arreglado el error del diseño de los frames

--- src/home/componentsIslands/SectionFrames.tsx
+++ src/home/componentsIslands/SectionFrames.tsx
@@ -1,27 +1,17 @@
 import type { FrameData } from '@/env'
 
-import { useEffect, useState } from 'react'
-
-// import { APIFrames } from '@/services/API'
+import { useState } from 'react'
 
 import './styles.css'
 
 export const SectionFrames = ({ framesData }: { framesData: FrameData }) => {
   const [frames] = useState<FrameData>(framesData)
 
-  // useEffect(() => {
-  //   const getFrames = async (): Promise<void> => {
-  //     const frameData = (await APIFrames.getFrames()) || []
-  //     setFrames(frameData)
-  //   }
-  //   getFrames()
-  // }, [])
-
   return (
     <div id="frames" className="min-h-[400px]">
       {frames.map(({ title, image }, index) => (
         <article
-          className="overflow-hidden rounded-md shadow-md hover:scale-[102%] hover:shadow-local_accent transition-all duration-300 aspect-square"
+          className="overflow-hidden rounded-md shadow-md hover:shadow-local_accent transition-all duration-300 aspect-square w-full h-full"
           key={title + index}
         >
           <img className="w-full h-full object-cover" src={image} alt={title} />
