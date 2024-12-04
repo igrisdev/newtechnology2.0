710bc48d41acb55c4501e51d965d73536ab58869
igrisdev <johanalvarezz237@gmail.com>
2024-12-04 13:26:05 -0500

    Mejora de la seccion de servicios

--- src/components/home/componentsIslands/SlideHome.tsx
+++ src/components/home/componentsIslands/SlideHome.tsx
@@ -1,5 +1,7 @@
 import { Swiper, SwiperSlide } from 'swiper/react'
 
+import { sendMessage } from "@utils/SendMessageWhatsApp"
+
 // Import Swiper styles
 import 'swiper/css'
 import 'swiper/css/pagination'
@@ -11,36 +13,48 @@ import './styles.css'
 import { Pagination, Navigation, Autoplay } from 'swiper/modules'
 
 export const SlideHome = () => {
+  const handelButtonWhatsApp = (message: string) => {
+    sendMessage(message)
+  }
+
   const services = [
     {
       title: 'Reparación',
       description: 'Reparación de equipos de computadoras y teléfonos móviles',
       image:
         'https://tecnosentry.com/wp-content/uploads/2024/08/cell-android-repair-1024x600-1.webp',
+      message: "hola"
     },
     {
       title: 'Venta Celulares',
       description: 'Venta de celulares de segunda mano',
       image:
         'https://http2.mlstatic.com/D_NQ_NP_663333-MCO75319299755_032024-O.webp',
+      href: "/all_products",
+      text: "Productos"
     },
     {
       title: 'Venta Accesorios',
       description: 'Venta de accesorios para computadoras y teléfonos móviles',
       image:
-        'https://www.tiendaonlinedetecnologia.com/wp-content/uploads/2021/05/IMPORTADORES-DIRECTOS-DE-ACCESORIOS-PARA-CELULARES-EN-BOGOTA-COLOMBIA-DESDE-CHINA-8.jpeg',
+        'https://http2.mlstatic.com/D_NQ_NP_663333-MCO75319299755_032024-O.webp',
+      href: "/all_products",
+      text: "Productos"
     },
     {
       title: 'Venta Repuestos',
       description: 'Venta de repuestos para computadoras y teléfonos móviles',
       image:
         'https://image.made-in-china.com/202f0j00jkfoSlsnGvce/Hot-Selling-New-Brand-Repuestos-PARA-Cellular-Smart-Phone-LCD-Screen-Replacement-for-Infinixn-Hot-12-PRO.webp',
+      href: "/all_products",
+      text: "Productos"
     },
     {
       title: 'Cuadros Personalizados',
       description: 'Venta de cuadros personalizados',
       image:
         'https://m.media-amazon.com/images/I/61h6v7CNvUL._UF1000,1000_QL80_.jpg',
+      message: "hola"
     },
   ]
 
@@ -79,7 +93,7 @@ export const SlideHome = () => {
         navigation={true}
         className="mySwiper"
       >
-        {services.map(({ title, description, image }, index) => (
+        {services.map(({ title, description, image, message, text, href }, index) => (
           <SwiperSlide key={title + index}>
             <article className="flex flex-col h-[450px]">
               <div className="min-h-[270px] max-h-[225px] rounded-md overflow-hidden">
@@ -99,14 +113,41 @@ export const SlideHome = () => {
                   </h2>
                   <p className="text-local_text mt-2 text-sm">{description}</p>
                 </div>
-                <button className="w-max bg-local_button text-local_title_2 text-xl font-bold py-2 px-4 rounded-md">
-                  Read more
-                </button>
+                {message ? (
+                  <button
+                    onClick={() => handelButtonWhatsApp('f')}
+                    className="w-max gap-2 bg-local_accent rounded-md px-5 py-2 text-local_text_2 font-semibold flex"
+                  >
+                    <p className="text-lg">Contactar</p>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      className="icon icon-tabler icons-tabler-outline icon-tabler-brand-whatsapp"
+                    ><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path
+                      d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9"></path><path
+                        d="M9 10a.5 .5 0 0 0 1 0v-1a.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a.5 .5 0 0 0 0 -1h-1a.5 .5 0 0 0 0 1"
+                      ></path></svg
+                    >
+                  </button>
+                ) : (
+                  <a href={href}
+                    className="w-max gap-2 bg-local_accent rounded-md px-5 py-2 text-local_text_2 font-semibold flex"
+                  >
+                    <p className="text-lg">{text}</p>
+                  </a>
+                )}
               </div>
             </article>
           </SwiperSlide>
         ))}
-      </Swiper>
+      </Swiper >
     </>
   )
 }
